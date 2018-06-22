6fcfcdae20e5196592ae2681310370b365f8f9c3
Aaron-Bird <25508292+Aaron-Bird@users.noreply.github.com>
2018-06-22 12:16:57 +0800

    025 Remove Duplicates from Sorted Array

---
+++ JavaScript/025 Remove Duplicates from Sorted Array.js
@@ -0,0 +1,59 @@
+// Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+
+// Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+// Example 1:
+
+// Given nums = [1,1,2],
+
+// Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+
+// It doesn't matter what you leave beyond the returned length.
+// Example 2:
+
+// Given nums = [0,0,1,1,1,2,2,3,3,4],
+
+// Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
+
+// It doesn't matter what values are set beyond the returned length.
+// Clarification:
+
+// Confused why the returned value is an integer but your answer is an array?
+
+// Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
+
+// Internally you can think of this:
+
+// // nums is passed in by reference. (i.e., without making a copy)
+// int len = removeDuplicates(nums);
+
+// // any modification to nums in your function would be known by the caller.
+// // using the length returned by your function, it prints the first len elements.
+// for (int i = 0; i < len; i++) {
+//     print(nums[i]);
+// }
+
+// 维护两个指针,用第二个指针从前向后遍历数组
+// 如果两个指针指向的数值不同,则将前一个加1,然后将后一个指针指向的数值复制到前一个指针的位置上
+// 返回非重复部分的长度即可,无需处理后边剩下的重复数字
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    if (nums.length < 2) return nums.length;
+    let i = 0;
+    let j = 1;
+    while (j < nums.length) {
+        if (nums[i] !== nums[j]) {
+            i++;
+            nums[i] = nums[j]
+        }
+        j++;
+    }
+    return i + 1;
+};
+
+// test
+console.log(removeDuplicates([1,1,2]));
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
