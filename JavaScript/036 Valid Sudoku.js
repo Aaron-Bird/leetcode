2720d3458f115d969da068207940a5911d3e37b8
Aaron-Bird <25508292+Aaron-Bird@users.noreply.github.com>
2018-07-06 23:12:30 +0800

    036 Valid Sudoku

---
+++ JavaScript/036 Valid Sudoku.js
@@ -0,0 +1,122 @@
+// Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+// 1 Each row must contain the digits 1-9 without repetition.
+// 2 Each column must contain the digits 1-9 without repetition.
+// 3 Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+// ![](https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Sudoku-by-L2G-20050714.svg/250px-Sudoku-by-L2G-20050714.svg.png)
+
+// The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+
+// Example 1:
+
+// Input:
+// [
+//   ["5","3",".",".","7",".",".",".","."],
+//   ["6",".",".","1","9","5",".",".","."],
+//   [".","9","8",".",".",".",".","6","."],
+//   ["8",".",".",".","6",".",".",".","3"],
+//   ["4",".",".","8",".","3",".",".","1"],
+//   ["7",".",".",".","2",".",".",".","6"],
+//   [".","6",".",".",".",".","2","8","."],
+//   [".",".",".","4","1","9",".",".","5"],
+//   [".",".",".",".","8",".",".","7","9"]
+// ]
+// Output: true
+// Example 2:
+
+// Input:
+// [
+//   ["8","3",".",".","7",".",".",".","."],
+//   ["6",".",".","1","9","5",".",".","."],
+//   [".","9","8",".",".",".",".","6","."],
+//   ["8",".",".",".","6",".",".",".","3"],
+//   ["4",".",".","8",".","3",".",".","1"],
+//   ["7",".",".",".","2",".",".",".","6"],
+//   [".","6",".",".",".",".","2","8","."],
+//   [".",".",".","4","1","9",".",".","5"],
+//   [".",".",".",".","8",".",".","7","9"]
+// ]
+// Output: false
+// Explanation: Same as Example 1, except with the 5 in the top left corner being 
+//     modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+// Note:
+
+// A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+// Only the filled cells need to be validated according to the mentioned rules.
+// The given board contain only digits 1-9 and the character '.'.
+// The given board size is always 9x9.
+
+
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function (board) {
+    
+    let getMap = function(num) {
+        arr = [];
+        for (var i = 0; i < num; i++) {
+            arr.push([]);
+        }
+        return arr;
+    }
+    let rowMap = getMap(9),
+        columMap = getMap(9),
+        boxMap = getMap(9);
+    console.log(rowMap)
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            let num = board[i][j];
+            if (num === '.') continue;
+
+            box = Math.floor(i / 3) * 3 + Math.floor(j / 3);
+            if (rowMap[i][num] || columMap[j][num] || boxMap[box][num]) {
+                console.log(i, j)
+                return false;
+            } else {
+                rowMap[i][num] = 1;
+                columMap[j][num] = 1;
+                boxMap[box][num] = 1;
+            }
+        }
+    }
+    return true;
+};
+
+// test
+let board = [
+    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+];
+console.log(isValidSudoku(board));
+// board = [
+//     ["8", "3", ".", ".", "7", ".", ".", ".", "."],
+//     ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+//     [".", "9", "8", ".", ".", ".", ".", "6", "."],
+//     ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+//     ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+//     ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+//     [".", "6", ".", ".", ".", ".", "2", "8", "."],
+//     [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+//     [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+// ];
+// console.log(isValidSudoku(board));
+// board = [
+//     [".", ".", ".", ".", "5", ".", ".", "1", "."],
+//     [".", "4", ".", "3", ".", ".", ".", ".", "."],
+//     [".", ".", ".", ".", ".", "3", ".", ".", "1"],
+//     ["8", ".", ".", ".", ".", ".", ".", "2", "."],
+//     [".", ".", "2", ".", "7", ".", ".", ".", "."],
+//     [".", "1", "5", ".", ".", ".", ".", ".", "."],
+//     [".", ".", ".", ".", ".", "2", ".", ".", "."],
+//     [".", "2", ".", "9", ".", ".", ".", ".", "."],
+//     [".", ".", "4", ".", ".", ".", ".", ".", "."]
+// ];
+// console.log(isValidSudoku(board));
---
+++ Python/036 Valid Sudoku.py
@@ -0,0 +1,113 @@
+# Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+# 1 Each row must contain the digits 1-9 without repetition.
+# 2 Each column must contain the digits 1-9 without repetition.
+# 3 Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+# ![](https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Sudoku-by-L2G-20050714.svg/250px-Sudoku-by-L2G-20050714.svg.png)
+
+# The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+
+# Example 1:
+
+# Input:
+# [
+#   ["5","3",".",".","7",".",".",".","."],
+#   ["6",".",".","1","9","5",".",".","."],
+#   [".","9","8",".",".",".",".","6","."],
+#   ["8",".",".",".","6",".",".",".","3"],
+#   ["4",".",".","8",".","3",".",".","1"],
+#   ["7",".",".",".","2",".",".",".","6"],
+#   [".","6",".",".",".",".","2","8","."],
+#   [".",".",".","4","1","9",".",".","5"],
+#   [".",".",".",".","8",".",".","7","9"]
+# ]
+# Output: true
+# Example 2:
+
+# Input:
+# [
+#   ["8","3",".",".","7",".",".",".","."],
+#   ["6",".",".","1","9","5",".",".","."],
+#   [".","9","8",".",".",".",".","6","."],
+#   ["8",".",".",".","6",".",".",".","3"],
+#   ["4",".",".","8",".","3",".",".","1"],
+#   ["7",".",".",".","2",".",".",".","6"],
+#   [".","6",".",".",".",".","2","8","."],
+#   [".",".",".","4","1","9",".",".","5"],
+#   [".",".",".",".","8",".",".","7","9"]
+# ]
+# Output: false
+# Explanation: Same as Example 1, except with the 5 in the top left corner being
+#     modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+# Note:
+
+# A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+# Only the filled cells need to be validated according to the mentioned rules.
+# The given board contain only digits 1-9 and the character '.'.
+# The given board size is always 9x9.
+
+
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        rowMap = [[0] * 10] * 9
+        columnMap = [[0] * 10] * 9
+        boxMap = [[0] * 10] * 9
+        for i in range(9):
+            for j in range(9):
+                num = board[i][j]
+                if num == '.':
+                    continue
+                else:
+                    num = int(num)
+                box = i // 3 * 3 + j // 3
+                if rowMap[i][num] or columnMap[j][num] or boxMap[box][num]:
+                    return False
+                else:
+                    rowMap[i][num] = 1
+                    columnMap[j][num] = 1
+                    boxMap[box][num] = 1
+        return True
+
+
+#  test
+s = Solution()
+board = [
+    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+]
+print(s.isValidSudoku(board))
+board = [
+    ["8", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+]
+print(s.isValidSudoku(board))
+board = [
+    [".", ".", ".", ".", "5", ".", ".", "1", "."],
+    [".", "4", ".", "3", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", "3", ".", ".", "1"],
+    ["8", ".", ".", ".", ".", ".", ".", "2", "."],
+    [".", ".", "2", ".", "7", ".", ".", ".", "."],
+    [".", "1", "5", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", "2", ".", ".", "."],
+    [".", "2", ".", "9", ".", ".", ".", ".", "."],
+    [".", ".", "4", ".", ".", ".", ".", ".", "."]
+]
+print(s.isValidSudoku(board))
