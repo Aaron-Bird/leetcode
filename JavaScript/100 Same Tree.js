288abbcb763eb39f3f17e61baf270ea798ef0ed3
Aaron-Bird <25508292+Aaron-Bird@users.noreply.github.com>
2018-08-12 16:38:57 +0800

    100 Same Tree

---
+++ JavaScript/100 Same Tree.js
@@ -0,0 +1,72 @@
+// Given two binary trees, write a function to check if they are the same or not.
+
+// Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+
+// Example 1:
+
+// Input:     1         1
+//           / \       / \
+//          2   3     2   3
+
+//         [1,2,3],   [1,2,3]
+
+// Output: true
+// Example 2:
+
+// Input:     1         1
+//           /           \
+//          2             2
+
+//         [1,2],     [1,null,2]
+
+// Output: false
+// Example 3:
+
+// Input:     1         1
+//           / \       / \
+//          2   1     1   2
+
+//         [1,2,1],   [1,1,2]
+
+// Output: false
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function (p, q) {
+    if (p === null && q === null) {
+        return true;
+    } else if (p === null || q === null) {
+        return false;
+    } else if (p.val === q.val) {
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    } else {
+        return false;
+    }
+};
+
+// test
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
+let p = new TreeNode(1);
+p.left = new TreeNode(2);
+p.right = new TreeNode(1);
+
+let q = new TreeNode(1);
+q.left = new TreeNode(2);
+q.right = new TreeNode(1);
+
+console.log(isSameTree(p, q));
---
+++ Python/100 Same Tree.py
@@ -0,0 +1,73 @@
+# Given two binary trees, write a function to check if they are the same or not.
+
+# Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+
+# Example 1:
+
+# Input:     1         1
+#           / \       / \
+#          2   3     2   3
+
+#         [1,2,3],   [1,2,3]
+
+# Output: true
+# Example 2:
+
+# Input:     1         1
+#           /           \
+#          2             2
+
+#         [1,2],     [1,null,2]
+
+# Output: false
+# Example 3:
+
+# Input:     1         1
+#           / \       / \
+#          2   1     1   2
+
+#         [1,2,1],   [1,1,2]
+
+# Output: false
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if p == q == None:
+            return True
+        elif p == None or q == None:
+            return False
+        elif p.val != q.val:
+            return False
+        else:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+# test
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+p = TreeNode(1)
+p.left = TreeNode(2)
+p.right = TreeNode(1)
+
+q = TreeNode(1)
+q.left = TreeNode(2)
+q.right = TreeNode(1)
+
+s = Solution()
+print(s.isSameTree(p, q))
